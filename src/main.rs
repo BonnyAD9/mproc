122911078eaf94b7da35bcd6651cd37f9dc88019
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-11-01 09:41:14 +0100

    Refactor measrement

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,13 @@
 use args::Args;
 use eyre::Result;
 use help::print_help;
+use measurement::Measurement;
 use pareg::Pareg;
-use std::{
-    io::{stderr, IsTerminal},
-    process::{Child, Command, ExitCode},
-    time::Duration,
-};
-use termal::eprintmcln;
+use std::process::ExitCode;
 
 mod args;
 mod help;
-
-#[cfg(target_os = "windows")]
-mod windows;
-
-#[cfg(target_os = "linux")]
-mod linux;
-
-struct Measurement {
-    time: Result<Duration>,
-    memory: Result<usize>,
-    exit_code: Option<i32>,
-}
+mod measurement;
 
 fn main() -> ExitCode {
     match start() {
@@ -47,83 +32,9 @@ fn start() -> Result<()> {
         return Ok(());
     };
 
-    let stats = measure_process(&program, &args.args)?;
+    let stats = Measurement::measure(&program, &args.args)?;
 
-    print_stats(&stats);
+    eprintln!("{stats}");
 
     Ok(())
-}
-
-fn measure_process(name: &String, args: &[String]) -> Result<Measurement> {
-    let mut proc = Command::new(name).args(args).spawn()?;
-
-    get_stats(&mut proc)
-}
-
-fn get_stats(proc: &mut Child) -> Result<Measurement> {
-    #[cfg(target_os = "windows")]
-    {
-        let res = proc.wait()?;
-        let (peak_memory, time) = windows::get_stats(proc);
-        Ok(Measurement {
-            time,
-            memory: peak_memory,
-            exit_code: res.code(),
-        })
-    }
-
-    #[cfg(target_os = "linux")]
-    {
-        let (peak_memory, time) = linux::get_stats(proc);
-        Ok(Measurement {
-            time,
-            memory: peak_memory,
-            exit_code: proc.wait()?.code(),
-        })
-    }
-}
-
-fn print_stats(stats: &Measurement) {
-    let color = stderr().is_terminal();
-
-    eprintmcln!(
-        color,
-        "===============<< {'y}mproc results{'_} >>==============="
-    );
-
-    match stats.time {
-        Ok(t) => eprintln!("Time: {:?}", t),
-        Err(_) => eprintln!("Failed to get time"),
-    }
-
-    eprintln!();
-
-    match stats.memory {
-        Ok(m) => eprintln!("Memory: {}", get_mem_string(m)),
-        Err(_) => eprintln!("Failed to get memory"),
-    }
-
-    eprintln!();
-
-    match stats.exit_code {
-        Some(e) => eprintln!("Exit code: {}", e),
-        None => eprintln!("No exit code"),
-    };
-}
-
-fn get_mem_string(mem: usize) -> String {
-    const UNITS: &[&str] = &["B", "KiB", "MiB", "GiB", "TiB", "EiB", "PiB"];
-
-    let mut level = 0;
-    let mut v = mem;
-    while v > 1024 {
-        level += 1;
-        v >>= 10;
-    }
-
-    format!(
-        "{} {}",
-        mem as f64 / (1 << (level * 10)) as f64,
-        UNITS[level]
-    )
 }
---
+++ src/measurement/mod.rs
@@ -0,0 +1,111 @@
+use std::{
+    fmt::Display,
+    io::{self, IsTerminal},
+    process::{Child, Command},
+    time::Duration,
+};
+
+use eyre::Result;
+use termal::writemcln;
+
+#[cfg(target_os = "windows")]
+mod windows;
+
+#[cfg(target_os = "linux")]
+mod linux;
+
+pub struct Measurement {
+    pub time: Result<Duration>,
+    pub memory: Result<usize>,
+    pub exit_code: Option<i32>,
+}
+
+impl Measurement {
+    pub fn measure(name: &String, args: &[String]) -> Result<Self> {
+        let mut proc = Command::new(name).args(args).spawn()?;
+
+        Self::get_stats(&mut proc)
+    }
+
+    pub fn get_stats(proc: &mut Child) -> Result<Self> {
+        #[cfg(target_os = "windows")]
+        {
+            let res = proc.wait()?;
+            let (peak_memory, time) = windows::get_stats(proc);
+            Ok(Measurement {
+                time,
+                memory: peak_memory,
+                exit_code: res.code(),
+            })
+        }
+
+        #[cfg(target_os = "linux")]
+        {
+            let (peak_memory, time) = linux::get_stats(proc);
+            Ok(Measurement {
+                time,
+                memory: peak_memory,
+                exit_code: proc.wait()?.code(),
+            })
+        }
+    }
+}
+
+impl Display for Measurement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let color = f
+            .precision()
+            .map(|p| p != 0)
+            .unwrap_or_else(|| io::stderr().is_terminal());
+
+        writemcln!(
+            f,
+            color,
+            "
+{'gr}===============<< {'y}mproc results {'gr}>>==============={'_}"
+        )?;
+
+        match self.time {
+            Ok(t) => writemcln!(f, color, "Time: {'w bold}{:?}{'_}\n", t)?,
+            Err(_) => writemcln!(f, color, "{'dr}Failed to get time{'_}\n")?,
+        }
+
+        match self.memory {
+            Ok(m) => writemcln!(
+                f,
+                color,
+                "Memory: {'w bold}{}{'_}\n",
+                get_mem_string(m)
+            )?,
+            Err(_) => writemcln!(f, color, "{'dr}Failed to get memory{'_}\n")?,
+        }
+
+        match self.exit_code {
+            Some(e) => {
+                if e == 0 {
+                    writemcln!(f, color, "Exit code: {'g bold}{}{'_}", e)
+                } else {
+                    writemcln!(f, color, "Exit code: {'r bold}{}{'_}", e)
+                }
+            }
+            None => writemcln!(f, color, "{'dr}No exit code{'_}"),
+        }
+    }
+}
+
+fn get_mem_string(mem: usize) -> String {
+    const UNITS: &[&str] = &["B", "KiB", "MiB", "GiB", "TiB", "EiB", "PiB"];
+
+    let mut level = 0;
+    let mut v = mem;
+    while v > 1024 {
+        level += 1;
+        v >>= 10;
+    }
+
+    format!(
+        "{} {}",
+        mem as f64 / (1 << (level * 10)) as f64,
+        UNITS[level]
+    )
+}
